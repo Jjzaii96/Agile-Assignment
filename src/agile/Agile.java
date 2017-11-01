befa798f36fcb2078c7cf64978a8b6988cf0b1f3
B1 <B1@B136>
2017-11-01 13:08:45 +0800

    my first try on

--- src/agile/Agile.java
+++ src/agile/Agile.java
@@ -15,7 +15,9 @@ public class Agile {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("main");
         // TODO code application logic here
+        System.out.println("haha");
     }
     
 }
